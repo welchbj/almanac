99fcb129e3b1dfe69934f2c98d68ec46aa523729
Brian Welch <welch18@vt.edu>
2019-07-09 21:48:46 -0400

    Add basic channel attributes to EvaluationContext

--- almanac/shell/evaluation_context.py
+++ almanac/shell/evaluation_context.py
@@ -2,6 +2,21 @@
 
 from __future__ import annotations
 
+from typing import (
+    Any,
+    Optional)
+
+from trio.abc import (
+    ReceiveChannel,
+    SendChannel)
+
+from .channels import (
+    NullReceiveChannel,
+    NullSendChannel)
+
+RecvAny = ReceiveChannel[Any]
+SendAny = SendChannel[Any]
+
 
 class EvaluationContext:
     """The context in which a part of an expression is evaluated.
@@ -9,14 +24,47 @@ class EvaluationContext:
     TODO: attributes
 
     """
-    # TODO
 
-    # TODO: how do we handle variables?
-    # TODO: how do we handle descriptors?
-    # TODO: references to prev/next tasks?
+    # TODO: how do we handle variable lookup?
+    # TODO: how do we handle descriptor lookup?
+
+    def __init__(
+        self,
+        stdin: Optional[RecvAny] = None,
+        stdout: Optional[SendAny] = None,
+        stderr: Optional[SendAny] = None
+    ) -> None:
+        self._stdin: RecvAny = (stdin if stdin is not None
+                                else NullReceiveChannel())
+        self._stdout: SendAny = (stdout if stdout is not None
+                                 else NullSendChannel())
+        self._stderr: SendAny = (stderr if stderr is not None
+                                 else NullSendChannel())
+        # TODO: initialization of descriptor and variable lookup tables
 
     def clone(
         self
     ) -> EvaluationContext:
         """Clone a deep copy of this ``EvaluationContext``."""
-        # TODO: this implementation will rely on the design of the class
+        # TODO: how do we handle cloning of streams?
+
+    @property
+    def stdin(
+        self
+    ) -> RecvAny:
+        """The channel representing this context's stdin stream."""
+        return self._stdin
+
+    @property
+    def stderr(
+        self
+    ) -> SendAny:
+        """The channel representing this context's stderr stream."""
+        return self._stderr
+
+    @property
+    def stdout(
+        self
+    ) -> SendyAny:
+        """The channel representing this context's stdout stream."""
+        return self._stdout
