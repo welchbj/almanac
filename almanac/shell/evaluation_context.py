52ae7b379519e19064b9a5a090af9ca6ececb51a
welchbj <12776611+welchbj@users.noreply.github.com>
2019-07-12 07:37:59 -0400

    Add implementation of EvaluationContext (minus clone())

--- almanac/shell/evaluation_context.py
+++ almanac/shell/evaluation_context.py
@@ -4,67 +4,107 @@ from __future__ import annotations
 
 from typing import (
     Any,
-    Optional)
+    MutableMapping,
+    Optional,
+    Union)
 
 from trio.abc import (
     ReceiveChannel,
     SendChannel)
+from trio import (
+    open_nursery)
 
 from .channels import (
     NullReceiveChannel,
     NullSendChannel)
 
-RecvAny = ReceiveChannel[Any]
-SendAny = SendChannel[Any]
+
+Channel = Union[SendChannel[Any], ReceiveChannel[Any]]
 
 
 class EvaluationContext:
     """The context in which a part of an expression is evaluated.
 
-    TODO: attributes
+    TODO: explain streams
 
     """
 
-    # TODO: how do we handle variable lookup?
-    # TODO: how do we handle descriptor lookup?
-
     def __init__(
         self,
-        stdin: Optional[RecvAny] = None,
-        stdout: Optional[SendAny] = None,
-        stderr: Optional[SendAny] = None
+        stdin: Optional[ReceiveChannel[Any]] = None,
+        stdout: Optional[SendChannel[Any]] = None,
+        stderr: Optional[SendChannel[Any]] = None
     ) -> None:
-        self._stdin: RecvAny = (stdin if stdin is not None
-                                else NullReceiveChannel())
-        self._stdout: SendAny = (stdout if stdout is not None
-                                 else NullSendChannel())
-        self._stderr: SendAny = (stderr if stderr is not None
-                                 else NullSendChannel())
-        # TODO: initialization of descriptor and variable lookup tables
+        self._stdin: ReceiveChannel[Any] = (stdin if stdin is not None
+                                            else NullReceiveChannel())
+        self._stdout: SendChannel[Any] = (stdout if stdout is not None
+                                          else NullSendChannel())
+        self._stderr: SendChannel[Any] = (stderr if stderr is not None
+                                          else NullSendChannel())
+
+        self._vars: MutableMapping[str, Any] = {}
+        self._descriptors: MutableMapping[str, Channel] = {
+            '0': self._stdin,
+            'stdin': self._stdin,
+            '1': self._stdout,
+            'stdout': self._stdout,
+            '2': self._stderr,
+            'stderr': self._stderr
+        }
 
     def clone(
         self
     ) -> EvaluationContext:
-        """Clone a deep copy of this ``EvaluationContext``."""
+        """Clone a deep copy of this :class:`EvaluationContext`."""
         # TODO: how do we handle cloning of streams?
+
+    @property
+    def vars(
+        self
+    ) -> MutableMapping[str, Channel]:
+        """A mapping of variable names to values."""
+        return self._vars
+
+    @property
+    def descriptors(
+        self
+    ) -> MutableMapping[str, Any]:
+        """A mapping of descriptor names to channel instances."""
+        return self._descriptors
 
     @property
     def stdin(
         self
-    ) -> RecvAny:
+    ) -> ReceiveChannel[Any]:
         """The channel representing this context's stdin stream."""
         return self._stdin
 
     @property
     def stderr(
         self
-    ) -> SendAny:
+    ) -> SendChannel[Any]:
         """The channel representing this context's stderr stream."""
         return self._stderr
 
     @property
     def stdout(
         self
-    ) -> SendAny:
+    ) -> SendChannel[Any]:
         """The channel representing this context's stdout stream."""
         return self._stdout
+
+    async def __aenter__(
+        self
+    ) -> None:
+        """Call `__aenter__` on all of this context's descriptors."""
+        with open_nursery() as nursery:
+            for descriptor in self._descriptors.values():
+                nursery.start_soon(descriptor.__aenter__)
+
+    async def __aexit__(
+        self
+    ) -> None:
+        """Call `__aclose__()` on all of this context's descriptors."""
+        with open_nursery() as nursery:
+            for descriptor in self._descriptors.values():
+                nursery.start_soon(descriptor.__aexit__)
