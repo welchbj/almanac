c9634fdf4c990749b4089d2b6367f651940f45e0
welchbj <12776611+welchbj@users.noreply.github.com>
2019-07-12 21:53:10 -0400

    Add impl for VariableShellArgument

--- almanac/shell/arguments/variable_shell_argument.py
+++ almanac/shell/arguments/variable_shell_argument.py
@@ -1,9 +1,14 @@
 """Implementation of the ``VariableShellArgument`` class."""
 
+from typing import (
+    Any)
+
 from .abstract_shell_argument import (
     AbstractShellArgument)
 from ..evaluation_context import (
     EvaluationContext)
+from ..shell_tokens import (
+    ShellTokens)
 
 
 class VariableShellArgument(AbstractShellArgument):
@@ -12,6 +17,12 @@ class VariableShellArgument(AbstractShellArgument):
     def evaluate(
         self,
         evaluation_context: EvaluationContext
-    ) -> str:
-        # TODO
-        raise NotImplementedError
+    ) -> Any:
+        var_name: str = self.raw_argument
+        if var_name.startswith(ShellTokens.VARIABLE_BEGIN_TOKEN):
+            var_name = var_name[1:]
+
+        try:
+            return evaluation_context.variables[var_name]
+        except KeyError as e:
+            raise ValueError(f'Unknown variable `{var_name}`') from e
