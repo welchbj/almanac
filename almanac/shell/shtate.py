518f6ea31f17ee1da2088224bb94cf502130caf4
welchbj <12776611+welchbj@users.noreply.github.com>
2019-07-13 08:24:48 -0400

    Move Shtate and ShlexerWarning classes into separate files

---
+++ almanac/shell/shtate.py
@@ -0,0 +1,27 @@
+"""Implementation of the ``Shtate`` class."""
+
+from enum import (
+    auto,
+    Enum)
+
+
+class Shtate(Enum):
+    """``Shlexer`` + state = ``Shtate``."""
+    ENTER_NEW_CONTEXT = auto()
+    IN_COMMAND_BODY = auto()
+
+    EXPECTING_FILE_DESCRIPTOR = auto()
+    IN_FILE_DESCRIPTOR = auto()
+
+    EXPECTING_HEREDOC_BEGIN = auto()
+    IN_HEREDOC = auto()
+
+    IN_HERESTR = auto()
+
+    IN_QUOTE_ALLOW_EXPANSION_BLOCK = auto()
+    IN_QUOTE_DISALLOW_EXPANSION_BLOCK = auto()
+
+    ESCAPE_NEXT = auto()
+
+    PARSE_FAILED = auto()
+    PARSE_COMPLETE = auto()
