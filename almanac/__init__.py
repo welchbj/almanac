eec0e14ebddfbe423e05b613b70089656d91c88b
welchbj <12776611+welchbj@users.noreply.github.com>
2019-07-14 09:25:01 -0400

    Add shell module to package-level exports

--- almanac/__init__.py
+++ almanac/__init__.py
@@ -3,4 +3,5 @@ from .commands import *  # noqa
 from .formatting import *  # noqa
 from .loaders import *  # noqa
 from .pages import  * # noqa
+from .shell import *  # noqa
 from .version import *  # noqa
