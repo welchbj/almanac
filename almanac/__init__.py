0c8ace5e08c5e60eaf2b3fec52eb950108540225
welchbj <12776611+welchbj@users.noreply.github.com>
2019-07-27 11:55:46 -0400

    Add full impl and tests for PagePath

--- almanac/__init__.py
+++ almanac/__init__.py
@@ -1,8 +1,9 @@
 from .app import *  # noqa
 from .commands import *  # noqa
+from .errors import *  # noqa
 from .formatting import *  # noqa
 from .loaders import *  # noqa
-from .pages import  * # noqa
+from .pages import * # noqa
 from .shell import *  # noqa
 from .utils import *  # noqa
 from .version import *  # noqa
---
+++ almanac/errors/positional_value_error.py
@@ -0,0 +1,20 @@
+"""Implementation of the ``PositionalValueError`` class."""
+
+
+class PositionalValueError(ValueError):
+    """An exception that holds a specific, error-causing input position."""
+
+    def __init__(
+        self,
+        msg: str,
+        error_pos: int
+    ) -> None:
+        super().__init__(msg)
+        self._error_pos = error_pos
+
+    @property
+    def error_pos(
+        self
+    ) -> int:
+        """The zero-based index of an error-causing position."""
+        return self._error_pos
