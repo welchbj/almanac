901d45243392896d4a8b76fa2216a25518eaaf99
Brian <welch18@vt.edu>
2019-12-07 15:54:28 -0500

    Remove dead code related to page serialization

--- almanac/pages/__init__.py
+++ almanac/pages/__init__.py
@@ -1,9 +1,5 @@
 from .abstract_page import (  # noqa
     AbstractPage)
-from .abstract_page_provider import (  # noqa
-    AbstractPageProvider)
-from .abstract_page_serializer_mixin import (  # noqa
-    AbstractPageSerializerMixin)
 from .directory_page import (  # noqa
     DirectoryPage)
 from .page_navigator import (  # noqa
--- almanac/pages/abstract_page_serializer_mixin.py
+++
@@ -1,8 +0,0 @@
-"""Implementation of the ``AbstractPageSerializerMixin`` class."""
-
-from abc import (
-    ABC)
-
-
-class AbstractPageSerializerMixin(ABC):
-    """The base class of all serializer mixins."""
