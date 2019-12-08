4207ff54ecfe45e04d21d06c457966d15086a60b
Brian <welch18@vt.edu>
2019-12-08 14:53:02 -0500

    Remove unused imports

--- almanac/commands/command_completer.py
+++ almanac/commands/command_completer.py
@@ -1,10 +1,3 @@
 """Implementation of the ``CommandCompleter`` class."""
 
-from prompt_toolkit.completion import (
-    CompleteEvent,
-    Completer,
-    Completion)
-from prompt_toolkit.document import (
-    Document)
-
 # TODO
