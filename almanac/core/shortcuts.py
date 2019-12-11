b3828ddee36bf01de8b3af7ed924a0a70f6b82de
Brian <welch18@vt.edu>
2019-12-10 19:30:01 -0500

    Add directory mode to make_standard_app

--- almanac/core/shortcuts.py
+++ almanac/core/shortcuts.py
@@ -4,20 +4,28 @@ from .application import (
     Application)
 from ..commands.builtins import (
     cd,
-    ls)
+    help,
+    ls,
+    quit)
 
 
 def make_standard_app(
+    directory_mode: bool = True
 ) -> Application:
     """Instantiate and configure a standard application.
 
-    This "standard" app will include the following:
-        * All builtin commands registered on the app
+    When directory mode is enabled, file-related commands will be registered
+    on the returned :class:`Application` instance. Otherwise, only a few
+    barebones commands are registered (quit, etc.).
 
     """
     app = Application()
 
-    app.command(cd)
-    app.command(ls)
+    app.command(help)
+    app.command(quit)
+
+    if directory_mode:
+        app.command(cd)
+        app.command(ls)
 
     return app
