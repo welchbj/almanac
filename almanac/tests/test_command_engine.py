bfcdecb8be7f1c48bfa50f928931185a1f20e8a4
welchbj <12776611+welchbj@users.noreply.github.com>
2020-07-22 07:23:17 -0400

    Add empty test file for CommandEngine

---
+++ almanac/tests/test_command_engine.py
@@ -0,0 +1,11 @@
+"""Tests for the ``CommandEngine`` class."""
+
+from unittest import TestCase
+
+# from almanac import CommandEngine
+
+
+class CommandEngineTestCase(TestCase):
+
+    def test_dummy(self):
+        assert False
