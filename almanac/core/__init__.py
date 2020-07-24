48d9267960f76fd0f6711a26c7315098c9776650
welchbj <12776611+welchbj@users.noreply.github.com>
2020-07-24 17:18:21 -0400

    Remove useless current app lock mechanism

--- almanac/core/__init__.py
+++ almanac/core/__init__.py
@@ -1,3 +1,3 @@
 from .application import Application  # noqa
-from .context import current_app, current_app_lock, set_current_app  # noqa
+from .context import current_app, set_current_app  # noqa
 from .shortcuts import make_standard_app  # noqa
