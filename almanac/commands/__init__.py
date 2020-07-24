1cc08796c19f453a1841f13eb5783c4191385e3c
welchbj <12776611+welchbj@users.noreply.github.com>
2020-07-24 10:33:19 -0400

    Add improved token-local parsing utilities

--- almanac/commands/__init__.py
+++ almanac/commands/__init__.py
@@ -1,4 +1,10 @@
+from .argument import Argument  # noqa
 from .command import Command  # noqa
+from .command_completer import CommandCompleter  # noqa
 from .command_engine import CommandEngine  # noqa
-from .parser import parse  # noqa
+from .parsing import (  # noqa
+    IncompleteToken,
+    last_incomplete_token,
+    parse_cmd_line
+)
 from .types import CommandCoroutine  # noqa
