c28e816a5ad3f91fc613193978ea2a68d39147a3
Brian <welch18@vt.edu>
2020-07-31 07:06:55 -0400

    Add basic cd and ls builtin command implementations

--- almanac/shortcuts/builtins.py
+++ almanac/shortcuts/builtins.py
@@ -7,12 +7,14 @@ from ..pages import PagePath
 from ..context import current_app
 
 
-async def cd(path: Optional[PagePath] = None) -> int:
+async def cd(path: PagePath) -> int:
     """Change directories."""
     app = current_app()
 
-    app.io.print_info('Called cd')
-    app.io.print_info('path:', path)
+    if path == '-':
+        app.page_navigator.back()
+    else:
+        app.page_navigator.change_directory(path)
 
     return ExitCodes.OK
 
@@ -27,7 +29,13 @@ async def help() -> int:
 async def ls(path: Optional[PagePath] = None) -> int:
     """List files in a directory."""
     app = current_app()
-    app.io.print_info('Called ls')
+
+    if path is None:
+        path = app.page_navigator.explode('.')
+
+    for child_page in app.page_navigator[path].children:
+        app.io.print_raw(child_page.path)
+
     return ExitCodes.OK
 
 
