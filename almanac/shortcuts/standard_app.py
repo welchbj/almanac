7c5af74d53b7ae9eab36513fd6bff52086e3a89e
welchbj <12776611+welchbj@users.noreply.github.com>
2020-08-01 15:22:19 -0400

    Rename io_ctx_cls kwarg to io_context_cls

--- almanac/shortcuts/standard_app.py
+++ almanac/shortcuts/standard_app.py
@@ -1,19 +1,25 @@
 """Shortcuts for initializing a pre-built application."""
 
+from typing import Type
+
 from prompt_toolkit.styles import Style
 
 from .builtins import cd, help, ls, quit
 from ..completion import PagePathCompleter, WordCompleter
 from ..core import Application
+from ..io import AbstractIoContext, StandardConsoleIoContext
 from ..pages import PagePath
 from ..style import DARK_MODE_STYLE
 
 
 def make_standard_app(
+    *,
     with_completion: bool = True,
     with_pages: bool = True,
     with_style: bool = True,
-    style: Style = DARK_MODE_STYLE
+    style: Style = DARK_MODE_STYLE,
+    io_context_cls: Type[AbstractIoContext] = StandardConsoleIoContext,
+    propagate_runtime_exceptions: bool = False,
 ) -> Application:
     """Instantiate and configure a standard application.
 
@@ -25,7 +31,9 @@ def make_standard_app(
     app = Application(
         with_completion=with_completion,
         with_style=with_style,
-        style=style
+        style=style,
+        io_context_cls=io_context_cls,
+        propagate_runtime_exceptions=propagate_runtime_exceptions
     )
 
     app.add_completers_for_type(bool, WordCompleter(['True', 'False']))
