e26b693466c1ce62e30db3b5b2c29ab82d1f3cf3
Brian <welch18@vt.edu>
2020-08-01 08:55:47 -0400

    Add utilities for simpler application-level testing

--- almanac/io/__init__.py
+++ almanac/io/__init__.py
@@ -1,2 +1,3 @@
 from .abstract_io_context import AbstractIoContext  # noqa
+from .null_io_context import NullIoContext  # noqa
 from .standard_console_io_context import StandardConsoleIoContext  # noqa
