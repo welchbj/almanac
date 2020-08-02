0aff23f3035fb848f94a79dd1868ab3820dd02d0
welchbj <12776611+welchbj@users.noreply.github.com>
2020-08-02 18:00:55 -0400

    Add support for prompt customization

--- almanac/shortcuts/standard_app.py
+++ almanac/shortcuts/standard_app.py
@@ -6,10 +6,17 @@ from prompt_toolkit.styles import Style
 
 from .builtins import cd, help, ls, quit
 from ..completion import PagePathCompleter, WordCompleter
+from ..context import current_app
 from ..core import Application
 from ..io import AbstractIoContext, StandardConsoleIoContext
 from ..pages import PagePath
 from ..style import DARK_MODE_STYLE
+
+
+def _current_page_prompt_str(
+) -> str:
+    app = current_app()
+    return app.page_navigator.current_page.get_prompt()
 
 
 def make_standard_app(
@@ -46,6 +53,9 @@ def make_standard_app(
     if with_pages:
         app.add_completers_for_type(PagePath, PagePathCompleter())
         app.add_promoter_for_type(PagePath, PagePath)
+
+        register_prompt_str = app.prompt_str()
+        register_prompt_str(_current_page_prompt_str)
 
         register_command(cd)
         register_command(ls)
