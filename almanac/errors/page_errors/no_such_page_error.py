9dbbdb9f61a7c431b70b171de8637d639d4133de
Brian <welch18@vt.edu>
2020-08-04 08:35:53 -0400

    Refactor subclasses of KeyError to remove quotes in error messages

--- almanac/errors/page_errors/no_such_page_error.py
+++ almanac/errors/page_errors/no_such_page_error.py
@@ -5,25 +5,27 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from .base_page_error import BasePageError
+from ..generic_errors import AlmanacKeyError
 
 if TYPE_CHECKING:
     from ...pages import PagePathLike
 
 
-class NoSuchPageError(BasePageError, KeyError):
+class NoSuchPageError(BasePageError, AlmanacKeyError):
     """An exception type for attempting to access a non-existent page."""
 
     def __init__(
         self,
         path: PagePathLike
     ) -> None:
-        super().__init__(f'No such page {str(path)}')
+        path_str = str(path)
+        super().__init__(f'No such page {path_str}')
 
-        self._path = path
+        self._path = str(path)
 
     @property
     def path(
         self
-    ) -> PagePathLike:
+    ) -> str:
         """The path that generated this error."""
         return self._path
