f2d027bf495462b496decf999e4b706b466facbf
Brian <welch18@vt.edu>
2020-08-06 15:34:58 -0400

    Remove errant double space

--- almanac/errors/argument_errors/missing_arguments_error.py
+++ almanac/errors/argument_errors/missing_arguments_error.py
@@ -19,7 +19,7 @@ class MissingArgumentsError(BaseArgumentError):
         if not missing_args:
             msg = 'Missing required argument(s).'
         elif len(missing_args) == 1:
-            msg = f'Missing  required argument {missing_args[0]}.'
+            msg = f'Missing required argument {missing_args[0]}.'
         elif len(missing_args) == 2:
             msg = f'Missing required arguments {missing_args[0]} and {missing_args[1]}.'
         else:
